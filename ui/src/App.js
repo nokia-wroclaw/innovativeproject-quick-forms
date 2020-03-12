1ad3b6e88ee172be8273d4f52c14d745b48ead83
Dawid Motak <dawidmotak1@gmail.com>
2020-03-12 19:35:06 +0100

    removed useless stuff

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,71 +1,23 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
-import Button from '@material-ui/core/Button';
-import logo from './logo.svg';
 import SignIn from './components/SignIn';
 
-const apiUrl = '';
-
-
-
+const apiUrl = '/api';
 
 class App extends React.Component {
-  state = {
-    users: [],
-  };
-
   componentDidMount() {
-    this.loadUsers();
+    this.getForms();
   }
 
-  async createUser() {
-    await axios.get(`${apiUrl}/user-create`);
-    this.loadUsers();
-  }
-
-  async loadUsers() {
-    const res = await axios.get(`${apiUrl}/users`);
-    this.setState({
-      users: res.data,
-    });
+  async getForms() {
+    await axios.get(`${apiUrl}/forms`);
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-
           <SignIn />
-
-          <button type="button" onClick={() => this.createUser()}>
-            Create User
-          </button>
-          <p>Users list:</p>
-          <ul>
-            {this.state.users.map((user) => (
-              <li key={user._id}>
-                id:
-                {user._id}
-              </li>
-            ))}
-          </ul>
         </header>
       </div>
     );
