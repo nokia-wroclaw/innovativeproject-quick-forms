f4650b35b6dcd75c06e2c27538ec5b968d3e5ae8
Mateque89 <48804716+Mateque89@users.noreply.github.com>
2020-03-18 22:58:54 +0100

    Example form

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import UserForms from './components/UserForms';
 import SignIn from './components/SignIn';
+import { Container } from '@material-ui/core';
 
 const apiUrl = '/api';
 
@@ -19,8 +20,9 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <SignIn />
-          
-          <UserForms/>
+          <Container mt={8}
+          >   <UserForms/>
+          </Container>
         </header>
       </div>
     );
