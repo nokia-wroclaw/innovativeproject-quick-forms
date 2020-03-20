3059e42c6eaf0a0ac00fc26a22cd47b5c5057479
Dawid Motak <dawidmotak1@gmail.com>
2020-03-20 01:33:06 +0100

    added basic authentication

--- api/server.js
+++ api/server.js
@@ -4,11 +4,12 @@ const app = express();
 const cors = require('cors');
 const connectDb = require('./src/connection/connection');
 const formRoute = require('./src/routing/formRouting');
+const auth = require('./src/auth');
 
 app.use(cors());
 
 app.use('/api/forms', formRoute);
-
+app.use('/api/signin', auth);
 
 const PORT = process.env.PORT || 8080;
 
