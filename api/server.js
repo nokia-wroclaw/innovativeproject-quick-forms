82a319f1f3ae33b93738b6737f017850eebbaa4f
krystian <nlins8224@gmail.com>
2020-03-20 17:15:52 +0100

    removed unused code

--- api/server.js
+++ api/server.js
@@ -7,13 +7,6 @@ const formRoute = require('./src/routing/formRouting');
 //const auth = require('./src/auth');
 
 app.use(cors());
-/*
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-*/
 app.use('/api/forms', formRoute);
 //app.use('/api/signin', auth);
 
