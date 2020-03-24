c95b839b7ddaac501d4a4f00acd87c5ee8e4dc2b
krystian <nlins8224@gmail.com>
2020-03-24 16:51:54 +0100

    added delete operation

--- api/src/routing/formRouting.js
+++ api/src/routing/formRouting.js
@@ -6,8 +6,6 @@ const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-// todo delete
-
 router.get('/prototypes', async (req, res) => {
   try {
     const forms = await Form.find();
@@ -66,6 +64,24 @@ router.post('/filled-forms', async (req, res) => {
     res.status(201).json(savedForm);
   } catch (err) {
     res.status(400).json({ message: err });
+  }
+});
+
+router.delete('/filled-forms/:id', async(req, res) => {
+  try{
+      const removedForm = await Form.findByIdAndDelete(req.params.id);
+      res.status(200).json(removedForm);
+  } catch (err){
+    res.status(404).json({message: err});
+  }
+});
+
+router.delete('/prototypes/:id', async(req, res) => {
+  try{
+    const removedForm = await Form.findByIdAndDelete(req.params.id);
+    res.status(200).json(removedForm);
+  } catch (err){
+    res.status(404).json({message: err});
   }
 });
 
