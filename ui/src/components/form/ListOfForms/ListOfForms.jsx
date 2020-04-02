440b41899d09f1d7f9dd581d9815614c0ec8d916
Dawid Motak <dawidmotak1@gmail.com>
2020-04-02 22:13:49 +0200

    made SingleForm component look

--- ui/src/components/form/ListOfForms/ListOfForms.jsx
+++ ui/src/components/form/ListOfForms/ListOfForms.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
-import axios from "axios";
-import {SingleForm} from "../SingleForm/SingleForm";
+import axios from 'axios';
+import SingleForm from '../SingleForm/SingleForm';
+class ListOfForms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listOfForms: [],
+    };
+  }
 
-class ListOfForms extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            listOfForms: []
-        };
-    }
+  componentDidMount() {
+    axios
+      .get(`/api/forms/templates/`)
+      .then(response => {
+        this.setState({listOfForms: response.data});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    componentDidMount() {
-        axios
-            .get(`/api/forms/templates/`)
-            .then(response => {
-                this.setState({listOfForms : response.data});
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-
-    render(){
-        return(
-                <div>
-                    {this.state.listOfForms.map(index => (
-                        <SingleForm key={index._id} template = {index.template}/>
-                    ))}
-                </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.listOfForms.map(index => (
+          <SingleForm
+            key={index._id}
+            formID={index._id}
+            template={index.template}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ListOfForms;
