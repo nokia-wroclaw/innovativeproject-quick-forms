b42ee29152886083a95549599c3c87a5096e7d09
krystian <nlins8224@gmail.com>
2020-04-02 15:54:16 +0200

    added auth routing, users routing and user model

--- api/server.js
+++ api/server.js
@@ -6,11 +6,13 @@ const connectDb = require('./src/connection/connection');
 const filledFormsRoute = require('./src/routing/filledForms');
 const templatesRoute = require('./src/routing/templateForms');
 const auth = require('./src/authorization/auth');
+const users = require('./src/routing/users');
 
 app.use(cors());
 app.use('/api/forms/templates', templatesRoute);
 app.use('/api/forms/filled-forms', filledFormsRoute);
-app.use('/api/signin/auth', auth);
+app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 8080;
 
