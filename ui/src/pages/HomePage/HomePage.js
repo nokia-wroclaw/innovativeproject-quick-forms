9b49ffeaaebde11861e807e9553847187d217822
Dawid Motak <dawidmotak1@gmail.com>
2020-04-16 23:20:02 +0200

    done about page

--- ui/src/pages/HomePage/HomePage.js
+++ ui/src/pages/HomePage/HomePage.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MainCard from './MainCard';
+import Content from './Content';
+import Sidebar from './Sidebar';
 import NavBar from '../NavBar';
 
-const HomePage = () => (
-  <div className="App">
-    <NavBar title="WELCOME" />
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
-export default HomePage;
+const mainCard = {
+  title: 'What is it?',
+  description:
+    "Quick forms is an app that let you automate the process of forms filling. You can easily create forms and generate QR codes, so sharing them with people who should complete the forms that you want is effortless",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+};
+
+
+const sidebar = {
+  title: 'About',
+  description:
+    'The app was developed by a group of three students as a part of an Innovative Projects by Nokia initiative',
+  source: [
+    { name: 'GitHub', icon: GitHubIcon, link: "https://github.com/nokia-wroclaw/innovativeproject-quick-forms" },
+  ],
+};
+
+export default function HomePage() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <NavBar title="WELCOME" />
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <MainCard card={mainCard} />
+          
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Content title="How to use the app?" />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              source={sidebar.source}
+            />
+          </Grid>
+        </main>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ ui/src/pages/HomePage/MainCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  mainCard: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginTop: 10,
+    marginBottom: theme.spacing(4),
+    backgroundImage: `url(${require("../../images/pen.jpg")})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainCardContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+}));
+
+export default function MainCard(props) {
+  const classes = useStyles();
+  const { card } = props;
+
+  return (
+    <Paper className={classes.mainCard}>
+      
+      <div className={classes.overlay} />
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.mainCardContent}>
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              {card.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {card.description}
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+MainCard.propTypes = {
+  post: PropTypes.object,
+};
