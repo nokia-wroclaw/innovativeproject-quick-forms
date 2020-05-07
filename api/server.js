9451eea59a20b4e4e145733ccbc83949d783ec26
krystian <nlins8224@gmail.com>
2020-05-07 17:35:45 +0200

    small fix

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const googleAuthRoute = require('./src/routing/googleAuth');
 const registerRoute = require('./src/routing/register');
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: [process.env.CLIENT_API_URL],
   allowedHeaders: [
     'Content-Type',
     'Authorization',
