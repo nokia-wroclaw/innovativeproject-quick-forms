5cec4124ba07046b070c99923c1a44caa0087677
krystian <nlins8224@gmail.com>
2020-06-06 21:54:04 +0200

    created basic tests

--- api/test/socketFormCommandCreate.test.js
+++ api/test/socketFormCommandCreate.test.js
@@ -1,14 +1,7 @@
-const  {beforeAll, afterAll, afterEach, beforeEach} = require( "@jest/globals");
+const  {beforeAll} = require( "@jest/globals");
 const mongoose = require('mongoose');
 const Form = require('../src/models/filledForm.model');
 const pendingForm = mongoose.model('pendingforms', Form);
-const filledForm = mongoose.model('filledforms', Form);
-const socketFormCommandCreate = require('../src/sockets/socketFormCommandCreate')
-
-const { DATABASE_SECRET } = process.env;
-const connection =
-    DATABASE_SECRET;
-
 
     beforeAll(async () => {
         await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true,
@@ -18,8 +11,4 @@ const connection =
                 process.exit(1);
             }
         });
-    });
-
-    test('Form is created on create command', async () => {
-
     });
