bb0d63a80cfbbb339c68d6694ae5bd9b06759d4c
Dawid Motak <dawidmotak1@gmail.com>
2020-06-10 23:58:58 +0200

    google auth fixes

--- api/src/authentication/passportGoogleStrategy.js
+++ api/src/authentication/passportGoogleStrategy.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, CLIENT_API_URL } = process.env;
 const User = require('../models/user.model');
 
 const verifyUser = (id, email, done) => {
@@ -25,7 +25,7 @@ const verifyUser = (id, email, done) => {
 const strategyOptions = {
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: 'api/auth/google/redirect'
+  callbackURL: `${CLIENT_API_URL}/api/auth/google/redirect`
 };
 
 const verifyCallback = async (accessToken, refreshToken, profile, done) => {
